61e2a3f2d52c13e3e6df1428d71eda856c99cc15
ibrahi4 <ibrabdulnasser@gmail.com>
2026-09-02 14:56:26 +0300

    Perf: Delete heavy PNG/JPEGs and reduce payload from 7.6MB to 400KB

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = 'Tajwedo Institute';
 export const SITE_URL = 'https://tajwedo.com';
-export const WHATSAPP_NUMBER = '+201091857418';
-export const WHATSAPP_LINK = 'https://wa.me/201091857418';
+export const WHATSAPP_NUMBER = '+201065247270';
+export const WHATSAPP_LINK = 'https://wa.me/201065247270';
 export const TELEGRAM_LINK = 'https://t.me/TajwedoInstitute';
 export const EMAIL = 'tajwedoinstitute@gmail.com';
